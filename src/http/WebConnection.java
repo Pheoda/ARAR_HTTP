9753c2c4d0d285106b64aa52141b3d0c9da0931a
Pheoda <n.fondacci@gmail.com>
2017-05-24 11:35:10 +0200

    Ajout extension de fichier

--- src/http/WebConnection.java
+++ src/http/WebConnection.java
@@ -1,9 +1,9 @@
 package http;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +12,15 @@ public class WebConnection extends Connection {
     public WebConnection(Socket connexion) {
         super(connexion);
     }
+    
+    public byte[] fillBody(String fileName, String value, String reason) {
+        byte[] body;
+        value = "200";
+        reason = "OK";
+        
+        
+        return body;
+    }
 
     @Override
     public void run() {
@@ -19,13 +28,17 @@ public class WebConnection extends Connection {
             try {
                 // Lecture de la requête envoyée par le client
                 int character, counter = 0;
-                boolean receivedCR = false;
+                boolean receivedCR = false, continueStartLine = true;
                 String request = new String();
                         
                 do
                 {
                     character = bufIn.read();
-                    System.out.print((char)character);
+                    
+                    //System.out.print((char)character);
+                    
+                    if(continueStartLine)
+                        request += (char)character;
                     
                     if(character == CR)
                         receivedCR = true;
@@ -35,21 +48,50 @@ public class WebConnection extends Connection {
                     }
                     if(character == LF && receivedCR) {
                         receivedCR = false;
+                        continueStartLine = false; // On arrête la lecture de la première ligne
                         counter++;
                     }
                 }while(counter != 2); // Fin par double CR LF
                 
                 // Analyse requête reçue
                 String[] strings = request.split(" "); // Découpage de la requête
+                
+                ArrayList<String> headers = new ArrayList<>();
+
                 switch(strings[0]) {
                     case "GET" :
+                        // On traite la ressource demandée
+                        if(strings[1].equals("/")) // On demande index.html
+                            strings[1] = "/index.html";
+                        
+                        String[] splitFile = strings[1].split("\\.");
+                        String extension = splitFile[splitFile.length-1];
+                        switch(extension) {
+                            case "html":
+                                headers.add("Content-Type: text/html");
+                                break;
+                            case "png":
+                                headers.add("Content-Type: image/png");
+                                break;
+                            case "jpg":
+                                headers.add("Content-Type: image/jpg");
+                                break;
+                        }
+                        break;
+                    case "HEAD":
                         break;
                 }
                 
+                String returnValue = new String();
+                String returnReason = new String();
+                
+                byte[] body = fillBody(strings[1], returnValue, returnReason);
+                // CreateResponseStartLine
+                // Sinon, parcourir fichier, remplir body
+                
                 // Réponse envoyée au client
-                byte[] startLine = createResponseStartLine("HTTP/1.1", "200", "OK");
-                ArrayList<String> headers = new ArrayList<>();
-                byte[] body = ("<!doctype html>\n" +
+                byte[] startLine = createResponseStartLine("HTTP/1.1", returnValue, returnReason);
+                /*byte[] body = ("<!doctype html>\n" +
                     "<html>\n" +
                     "<head>\n" +
                     "  <meta charset=\"utf-8\">\n" +
@@ -59,8 +101,7 @@ public class WebConnection extends Connection {
                     "<h1>LE SUPER SITE D'ALAIN</h1>" +
                     "<p><img src='images/alain.png'/></p>\n" +
                     "</body>\n" +
-                    "</html>").getBytes();
-                headers.add("Content-Type: text/html");
+                    "</html>").getBytes();*/
                 
                 byte[] message = createHTTPMessage(startLine, headers, body);
                 /*System.out.println();

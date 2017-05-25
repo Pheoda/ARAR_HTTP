98c9bcb628b63a0a921156f0f9c4907b8604e011
Nicolas Fondacci <nicolas.fondacci@etu.univ-lyon1.fr>
2017-05-25 12:57:04 +0200

    Ajout test index.html et design.css, fin fonction fillBody

--- src/http/WebConnection.java
+++ src/http/WebConnection.java
@@ -1,9 +1,11 @@
 package http;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,61 +14,88 @@ public class WebConnection extends Connection {
     public WebConnection(Socket connexion) {
         super(connexion);
     }
-    
+
     public byte[] fillBody(String fileName, String value, String reason) {
         byte[] body;
-        value = "200";
-        reason = "OK";
+     
+        fileName = System.getProperty("user.dir") + fileName;
+        File f = new File(fileName);
+
+        System.out.println("Traitement : " + f.getAbsolutePath());
+
         
+        if (!f.exists()) { // Erreur 404
+            value = "404";
+            reason = "Page non trouvée";
+            System.out.println("ERREUR 404");
+            return null;
+        } else {
+            value = "200";
+            reason = "OK";
+
+            body = new byte[(int) f.length()];
+
+            try {
+                FileInputStream file = new FileInputStream(f);
+                for (int i = 0; i < (int) f.length(); i++) {
+                    body[i] = (byte) file.read();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(WebConnection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         
+        System.out.println(new String(body));
+
         return body;
     }
 
     @Override
     public void run() {
-        while(true) {
+        while (true) {
             try {
                 // Lecture de la requête envoyée par le client
                 int character, counter = 0;
                 boolean receivedCR = false, continueStartLine = true;
                 String request = new String();
-                        
-                do
-                {
+
+                do {
                     character = bufIn.read();
-                    
+
                     //System.out.print((char)character);
-                    
-                    if(continueStartLine)
-                        request += (char)character;
-                    
-                    if(character == CR)
+                    if (continueStartLine) {
+                        request += (char) character;
+                    }
+
+                    if (character == CR) {
                         receivedCR = true;
-                    else if(character != LF) {
+                    } else if (character != LF) {
                         receivedCR = false;
                         counter = 0;
                     }
-                    if(character == LF && receivedCR) {
+                    if (character == LF && receivedCR) {
                         receivedCR = false;
                         continueStartLine = false; // On arrête la lecture de la première ligne
                         counter++;
                     }
-                }while(counter != 2); // Fin par double CR LF
-                
+                } while (counter != 2); // Fin par double CR LF
+
                 // Analyse requête reçue
                 String[] strings = request.split(" "); // Découpage de la requête
-                
+
                 ArrayList<String> headers = new ArrayList<>();
 
-                switch(strings[0]) {
-                    case "GET" :
+                switch (strings[0]) {
+                    case "GET":
                         // On traite la ressource demandée
-                        if(strings[1].equals("/")) // On demande index.html
+                        if (strings[1].equals("/")) // On demande index.html
+                        {
                             strings[1] = "/index.html";
-                        
+                        }
+
                         String[] splitFile = strings[1].split("\\.");
-                        String extension = splitFile[splitFile.length-1];
-                        switch(extension) {
+                        String extension = splitFile[splitFile.length - 1];
+                        switch (extension) {
                             case "html":
                                 headers.add("Content-Type: text/html");
                                 break;
@@ -81,28 +110,15 @@ public class WebConnection extends Connection {
                     case "HEAD":
                         break;
                 }
-                
+
                 String returnValue = new String();
                 String returnReason = new String();
-                
+
                 byte[] body = fillBody(strings[1], returnValue, returnReason);
-                // CreateResponseStartLine
-                // Sinon, parcourir fichier, remplir body
-                
+
                 // Réponse envoyée au client
                 byte[] startLine = createResponseStartLine("HTTP/1.1", returnValue, returnReason);
-                /*byte[] body = ("<!doctype html>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "  <meta charset=\"utf-8\">\n" +
-                    "  <title>Hello</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "<h1>LE SUPER SITE D'ALAIN</h1>" +
-                    "<p><img src='images/alain.png'/></p>\n" +
-                    "</body>\n" +
-                    "</html>").getBytes();*/
-                
+
                 byte[] message = createHTTPMessage(startLine, headers, body);
                 /*System.out.println();
                 System.out.println("REPONSE : ");
@@ -114,5 +130,5 @@ public class WebConnection extends Connection {
             }
         }
     }
-    
+
 }
